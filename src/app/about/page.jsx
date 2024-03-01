e11eccdb562de0b62bcdb7547a841e7ac4c7cbaa
erickubuntu <gestionecesaria@gmail.com>
2024-03-01 09:46:37 -0400

    1 de mar 2024

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h2>SOY About</h2>
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import st from "./page.module.css";
+import { useEffect } from "react";
+import Image from "next/image";
+
+export default function Home() {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.js");
+  }, []);
+
+  return (
+    <>
+      <div
+        className="container d-flex justify-content-end "
+        style={{ marginTop: "90px", textAlign: "justify", textIndent: "3em" }}
+      >
+        <div className="w-75">
+          <h1>
+            <span className="bg-dark-subtle  text-white ">
+              Achieving perfection in design is not about adding more,
+            </span>{" "}
+            but about refining and distilling the design until it reaches its
+            most essential and impactful form.
+          </h1>
+          <div>
+            <strong>Clarity</strong>
+          </div>
+        </div>
+      </div>
+      <div className="container card bg-dark-subtle   my-5">
+        <img src="/prog.png" className="card-img" alt="..." height={600} />
+        <div className="card-img">
+          <h5 className="card-title d-none ">Card title</h5>
+          <p className="card-text d-none ">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p className="card-text d-none ">
+            <small>Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}

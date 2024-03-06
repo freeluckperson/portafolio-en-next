fc621f52be1e38306e2c9779ab726ea13e025dd0
erickubuntu <gestionecesaria@gmail.com>
2024-03-06 12:56:12 -0400

    6 de mar 2024 2do commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,6 @@
 export default function AboutPage() {
   return (
-    <div className="container" style={{ margin: "90px" }}>
+    <div className="container" style={{ margin: "90px", height: "100vh" }}>
       <h2>SOY About</h2>
     </div>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,41 +2,53 @@
 
 import { useEffect, useState } from "react";
 
-function Card({ project }) {
+function Card({ project, loading }) {
   const { title, description, image, url, deploy } = project;
   return (
-    <div className="col-md-4 mt-4">
-      <div className="card h-100">
-        <div className=" text-end m-1">
-          <a className="btn btn-outline-dark" href={url} target="_blank">
-            Repo
-          </a>
-          <a
-            className="btn btn-outline-dark mx-1"
-            href={deploy}
-            target="_blank"
-          >
-            Live
-          </a>
+    <>
+      {loading ? (
+        <div className="col-md-4 mt-4">
+          <div className="card h-100">
+            <div className=" text-end m-1">
+              <a className="btn btn-outline-dark" href={url} target="_blank">
+                Repo
+              </a>
+              <a
+                className="btn btn-outline-dark mx-1"
+                href={deploy}
+                target="_blank"
+              >
+                Live
+              </a>
+            </div>
+            <img src={image} alt="..." className="card-img-top" />
+            <div className="card-body">
+              <h2 className="card-title">{title}</h2>
+              <div className="card-text">{description.slice(0, 50)}...</div>
+            </div>
+          </div>
         </div>
-        <img src={image} alt="..." className="card-img-top" />
-        <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <div className="card-text">{description.slice(0, 50)}...</div>
+      ) : (
+        <div className="col-md-4 mt-4">
+          <div className="card h-100">
+            <div className="spinner-border text-primary">LOADING</div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
 export default function Portfolio() {
-  const [projects, setProjects] = useState(null);
-  console.log(projects);
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     fetch("/api/project")
       .then((response) => response.json())
       .then((json) => setProjects(json))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(true));
   }, []);
 
   return (
@@ -47,7 +59,7 @@ export default function Portfolio() {
       <div className="container">
         <div className="row">
           {projects?.map((project, i) => (
-            <Card key={i} project={project} />
+            <Card key={i} project={project} loading={loading} />
           ))}
         </div>
       </div>
